5313441a9eb7675c1e7139431d8afdaff5d1bb9c
caskercasker <casketcasker@gmail.com>
2019-12-27 01:13:42 +0900

    dd

--- test/src/Hello.java
+++ test/src/Hello.java
@@ -1,26 +1,53 @@
-class A{
-	int a;
-	int b;
-	static int c;
+//import test.Calculator;
+//import test.MovieVO;
+
+class MovieVO{
+	String title;
+	
+	public MovieVO(String a){
+		this.title = a;
+	}
+	
 }
 
-public class Hello {
-	public static void main(String[]args){
-//		int a = 1;
-//		for(int i = 0; i<10; i++) {
-//			a += i;
-//		}
-//		System.out.println(a);
-//	}
-		
-		A aa = new A();
-		A bb = new A();
-		aa.c = 1100;
-		
-		
-		System.out.println(aa.c);
-		System.out.println(bb.c);
-	}
+class Calculator {
+    int left, right;
+ 
+    public Calculator(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+}
 
+public class Hello{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Calculator c1 = new Calculator(10,20);
+		Calculator c2 = new Calculator (10,30);
+		MovieVO m1 = new MovieVO("aa");
+		MovieVO m2 = new MovieVO("bb");
+		MovieVO m3 = new MovieVO("cc");
+		
+//		System.out.println(c1.left);
+//		System.out.println(m3.title); //716361
+//		System.out.println(m2.title); //6ff3c5b5
+//		System.out.println(m1.title); //3764951d
+		//System.out.println(c1);
+		//System.out.println(m2);
+		//System.out.println(m1);
+		System.out.println(c2);
+		System.out.println(c1);
+		
+		//System.out.println(m3);
+		/*
+		 *  test.MovieVO@6ff3c5b5
+			test.MovieVO@3764951d
+			test.MovieVO@4b1210ee
+		 * 
+		 * 
+		 */
+	} 
 
 }
